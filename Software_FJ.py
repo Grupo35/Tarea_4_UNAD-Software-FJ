c1f2b3c82792a06738b27b6c02208e440af1b792
Aldair Sierra <aldast166@gmail.com>
2026-04-22 23:05:11 -0500

    test commit

---
+++ Software_FJ.py
@@ -0,0 +1,9 @@
+# ---------------- IMPORTACIONES ----------------
+import ttkbootstrap as ttk
+import tkinter as tk
+from ttkbootstrap.constants import *
+from tkinter import messagebox, simpledialog
+import json
+import os
+import re
+from datetime import datetime
