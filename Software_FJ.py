05a290dad582ae04c0222c13320ec69f2f0ec4ae
Aldair Sierra <aldast166@gmail.com>
2026-04-25 23:18:15 -0500

    clases hijas de servicios completadas probadas en consola --- agregado de clases cliente y repertorio para guardarlos al igual que un borrador del guradado del log

--- Software_FJ.py
+++ Software_FJ.py
@@ -9,6 +9,10 @@ import re
 from datetime import datetime
 from abc import ABC, abstractmethod
 
+  #-------------------------------------------------------------------
+  #-----------------------Servicios--------------------
+  #-------------------------------------------------------------------   
+
 #================= CLASE BASE =================
 class Servicio(ABC):
     def __init__(self, id, nombre):
@@ -169,7 +173,12 @@ class ServicioEquipo(Servicio):
         self.__cantidad = valor
             
     def calcular_costos(self):
-        return self.__dias * self.__tarifa_dia * self.__cantidad
+        try:
+            return self.__dias * self.__tarifa_dia * self.__cantidad
+
+        except Exception as e:
+            print("Error calculando costos:", e)
+            return 0
         
     def calcular_descuento(self, cupon=False, cliente_premium=False):
         costo = self.calcular_costos()
@@ -194,36 +203,234 @@ class ServicioEquipo(Servicio):
   #-----------------------Asesorías especializadas--------------------
   #-------------------------------------------------------------------   
   
-class ServivioAsesoria(Servicio):
+class ServicioAsesoria(Servicio):
     def __init__(self, id, nombre, tipo_asesoria, horas, tarifa_horas, nivel="básico"):
         super().__init__(id, nombre)
         
         self.__tipo_asesoria = tipo_asesoria
         self.horas = horas
         self.tarifa_horas = tarifa_horas
-        self._nivel = nivel
-             
-            
+        self.__nivel = nivel
         
+    @property
+    def tipo_asesoria(self):
+        return self.__tipo_asesoria
+    
+    @property
+    def horas(self):
+        return self.__horas
+    
+    @horas.setter
+    def horas(self, valor):
+         if not isinstance(valor, (int, float)):
+            raise ValueError("Las horas deben ser númericas")
+            
+         if valor <= 0:
+            raise ValueError("Las horas deben deben ser mayores a 0")
+            
+         self.__horas = valor
+         
+    @property
+    def tarifa_horas(self):
+        return self.__tarifa_horas
+    
+    @tarifa_horas.setter
+    def tarifa_horas(self, valor):
+         if not isinstance(valor, (int, float)):
+            raise ValueError("Tarifas deben ser númericas")
+            
+         if valor <= 0:
+            raise ValueError("Tarifas deben ser mayores a 0")
+            
+         self.__tarifa_horas = valor
+         
+    def calcular_costos(self):
+        return self.__horas * self.__tarifa_horas
+    
+    def calcular_descuento(self, cupon=False, cliente_premium=False):
+        costo = self.calcular_costos()
+        descuento = 0
+        
+        if cupon:
+            descuento += costo * 0.04
+                
+        if cliente_premium:
+            descuento += costo * 0.12
+            
+        return costo - descuento
+    
+    def describir(self):
+        return (f"{super().describir()} | "
+                f"Tipo: {self.__tipo_asesoria} | "
+                f"Horas: {self.__horas} | "
+                f"Nivel: {self.__nivel} | "
+                f"Total: {self.calcular_costos()}")
+        
+  #-------------------------------------------------------------------
+  #-----------------------Clientes--------------------
+  #-------------------------------------------------------------------   
+class Cliente:
+    def __init__(self, id, nombre, telefono, email, tipo="normal"):
+        self.id = id
+        self.__nombre = nombre
+        self.telefono = telefono
+        self.email = email
+        self.tipo = tipo
+        
+    @property
+    def id(self):
+        return self.__id
+    
+    @id.setter
+    def id(self, valor):
+        if not isinstance(valor, int) or valor <= 0:
+            raise ValueError("ID inválido")
+        
+        self.__id = valor 
+        
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @property
+    def telefono(self):
+        return self.__telefono
+    
+    @telefono.setter
+    def telefono(self, valor):
+        if not isinstance(valor, str) or not valor.isdigit():
+            raise ValueError("Teléfono debe contener solo números")
+                
+        self.__telefono = valor
+    
+    @property
+    def email(self):
+        return self.__email
+    
+    @email.setter
+    def email(self, valor):
+        if "@" not in valor:
+            raise ValueError("Email inválido")
+        
+        self.__email = valor
+        
+    @property
+    def tipo(self):
+        return self.__tipo
+    
+    @tipo.setter
+    def tipo(self, valor):
+        if valor not in ["normal", "premium"]:
+            raise ValueError("Tipo debe ser normal o premium")
+        
+        self.__tipo = valor    
+        
+    def es_premium(self):
+        return self.__tipo == "premium"
+    
+    def __str__(self):
+        return f"Cliente: {self.__nombre} | ID: {self.__id} | Tipo: {self.__tipo}"
     
     
-#test    
+ #-------------------------------------------------------------------
+  #-----------------------LOG-------------------
+  #-------------------------------------------------------------------   
+class Logger:
+    
+    FILE_PATH = "data/logs.txt"
+    
+    def __init__(self):
+        os.makedirs("data", exist_ok=True)
+        
+    def log(self, nivel, mensaje):
+        tiempo = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        with open(self.FILE_PATH, "a", encoding="utf-8") as f:
+            f.write(f"[{tiempo}] {nivel}: {mensaje}\n")
+    
+ #-------------------------------------------------------------------
+  #-----------------------Manejo de datos de clientes-------------------
+  #-------------------------------------------------------------------   
+class ClienteDuplicadoError(Exception):
+    pass
+
+
+class RepositorioError(Exception):
+    pass
+    
+class ClienteRepository:
+
+    FILE_PATH = "data/clientes.json"
+
+    def __init__(self):
+        os.makedirs("data", exist_ok=True)
+        self.logger = Logger()
+
+        if not os.path.exists(self.FILE_PATH):
+            with open(self.FILE_PATH, "w") as f:
+                json.dump([], f)
+
+    def get_clientes_registrados(self):
+        try:
+            with open(self.FILE_PATH, "r") as f:
+                return json.load(f)
+
+        except Exception as e:
+            self.logger.log("ERROR", f"Error leyendo clientes: {e}")
+            raise RepositorioError("No se pudieron leer clientes")
+
+    def save_cliente(self, cliente):
+
+        try:
+            with open(self.FILE_PATH, "r") as f:
+                data = json.load(f)
+
+            # duplicados
+            if any(c["id"] == cliente.id for c in data):
+                self.logger.log("ERROR", f"Cliente duplicado ID {cliente.id}")
+                raise ClienteDuplicadoError("Cliente ya existe")
+
+            data.append({
+                "id": cliente.id,
+                "nombre": cliente.nombre,
+                "telefono": cliente.telefono,
+                "email": cliente.email,
+                "tipo": cliente.tipo
+            })
+
+            with open(self.FILE_PATH, "w") as f:
+                json.dump(data, f, indent=4)
+
+        except ClienteDuplicadoError:
+            raise
+
+        except Exception as e:
+            self.logger.log("ERROR", f"Error guardando cliente: {e}")
+            raise RepositorioError("Error en almacenamiento")
+
+        else:
+            self.logger.log("INFO", f"Cliente guardado ID {cliente.id}")
+
+        finally:
+            print("Operación de guardado finalizada")
+    
+            
+#prueba en consola
+repo = ClienteRepository()
+
 try:
-    sala = ServicioSala(1101, "Sala 1", 2, 20000)
+    cliente1 = Cliente(1, "Alda", "300123", "mail@gmail.com", "premium")
 
-    print(sala.describir())
-    print("Costo final:", sala.calcular_descuento())
+    repo.save_cliente(cliente1)
 
-except ValueError as e:
-    print("ERROR CAPTURADO:", e)
-    
-#test 2
+except ClienteDuplicadoError as e:
+    print("DUPLICADO:", e)
 
-try:
-    s = ServicioEquipo(1104, "Microfono", "Altavoces", 2, 25000, 2)
-    
-    print(s.describir())
-    print("Costo total:", s.calcular_descuento())
-    
-except ValueError as t:
-    print("Errores en entrada:", t)
+except RepositorioError as e:
+    print("ERROR REPOSITORIO:", e)
+
+else:
+    print("Cliente guardado correctamente ✔")
+
+finally:
+    print("Fin del proceso")
